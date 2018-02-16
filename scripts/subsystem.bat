e1a77111cbae77d8320895c047e2c3a0e60dcae0
alex <web-webalexx@yandex.ru>
2018-02-16 16:36:47 +0100

    a test have been done

--- scripts/subsystem.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-mvn -v
-
-mvn archetype:generate -DarchetypeArtifactId=wildfly-subsystem -DarchetypeGroupId=org.wildfly.archetypes -DarchetypeVersion=8.0.0.Final -DarchetypeRepository=http://repository.jboss.org/nexus/content/groups/public
-
-mvn archetype:generate -DarchetypeArtifactId=jboss-as-subsystem -DarchetypeGroupId=org.jboss.as.archetypes -DarchetypeVersion=7.1.1.Final -DarchetypeRepository=http://repository.jboss.org/nexus/content/groups/public
